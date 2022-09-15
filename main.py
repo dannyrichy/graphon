f248a4b40b0e8929e55d7935386abe58dd69a5cc
simon <bonato@kth.se>
2022-09-15 11:32:06 +0200

    node2vec stuff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from utils import *
 
+from node2vec import Node2Vec
+import networkx as nx
+
 
 def combine_datasets(dataset_names=[]):
     result = []
@@ -19,7 +22,6 @@ def combine_datasets(dataset_names=[]):
 
 
 def main():
-
     DOWNLOAD_DATA = False
 
     if DOWNLOAD_DATA:
@@ -47,5 +49,40 @@ def main():
     # gt_fb_github_reddit_deezer
 
 
+def node2vec(graph, emb_dir=None, savename=None):
+
+    graph = nx.from_numpy_array(graph.numpy())
+
+    dimensions = 64
+    walk_length = 30
+    num_walks = 200
+    node2vec = Node2Vec(graph, dimensions=dimensions, walk_length=walk_length, num_walks=num_walks)
+
+    # saves the description of the parameters used for the embedding
+    with open(f'{emb_dir}/AA_emb_params.txt', 'w') as f:
+        f.write(f'dimensions --> {dimensions}\nwalk_length --> {walk_length}\nnum_walks --> {num_walks}')
+
+    model = node2vec.fit(window=10, min_count=1, batch_words=4)
+
+    if savename is not None:
+        model.wv.save_word2vec_format(f'{emb_dir}/{savename}')
+
+def embed_all_node2vec(emb_dir):
+    datasets = ['deezer_ego_nets', 'facebook_ct1', 'github_stargazers', 'REDDIT-BINARY']
+
+    for ds in datasets:
+        graphs = load_graph(min_num_nodes=10, name=ds)
+        for idx, g in enumerate(graphs):
+            savename = f'{ds}/{ds}_{idx}'
+            node2vec(graph=g, emb_dir=emb_dir, savename=savename)
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    embed_all_node2vec(emb_dir='node2vec_embeddings_0')
+
+
+
+
+
+
