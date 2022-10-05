079a60ca4e7afa81c553fb1dbb8c6d2b6d47c353
simon <bonato@kth.se>
2022-10-05 20:17:02 +0200

    remade main function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # from config import NUM_GRAPHONS, NUM_GRAPHS_PER_GRAPHONS, DATA, CREATE_EMBEDDINGS, EMBEDDING_DIR, SAVE_GRAPHONS, SAVE_GRAPHONS_LOC
-from graph2vec.utils import embed_all_graph2vec, load_embeddings
+from graph2vec.utils import embed_all_graph2vec, load_graphons_embeddings
 from graphon.hist_estimator import hist_approximate
 from sklearn.ensemble import RandomForestClassifier
 from graphon.graphons import SynthGraphons
@@ -33,58 +33,85 @@ def update_sweep_config(sweep_config, config_def):
 
 
 def clustering_classification(
-    NUM_GRAPHONS=1000,
     NUM_GRAPHS_PER_GRAPHON=100,
     NUM_NODES=None,
+    N0=30,
     SAVE_GRAPHONS=False,
     CREATE_EMBEDDINGS=False,
-    EMBEDDING_DIR=None,
-    DATA='',
+    G2V_EMBEDDING_DIR=None,
+    DATA=None,
     DOWNLOAD_DATA=False,
     SWEEP=False,
+    
 ):
-  
-    SAVE_GRAPHONS_LOC = f'./graphons_dir/{NUM_GRAPHONS}_graphons_{NUM_GRAPHS_PER_GRAPHON}_graphs.pkl'
-    syn_graphons = SynthGraphons(NUM_GRAPHS_PER_GRAPHON, DATA['SYNTHETIC_DATA'], num_nodes=NUM_NODES, save_graphons_loc = SAVE_GRAPHONS_LOC)
+
+    NUM_GRAPHONS = len(DATA[1])
+    GRAPHONS_DIR = f'./graphons_dir/{NUM_GRAPHONS}_graphons_{NUM_GRAPHS_PER_GRAPHON}_graphs.pkl'
+
+    syn_graphons = SynthGraphons(NUM_GRAPHS_PER_GRAPHON, 
+                                DATA[1], 
+                                num_nodes=NUM_NODES, 
+                                save_graphons_loc=GRAPHONS_DIR
+                                )
+    # generate graphs from graphons
     if SAVE_GRAPHONS:
-        print('storing graphs at ', SAVE_GRAPHONS_LOC)
+        print('storing graphs at ', GRAPHONS_DIR)
         graphs, labels = syn_graphons.data_simulation(start=100, stop=1000, save=SAVE_GRAPHONS)
     else:
-        print('loading graphs from ', SAVE_GRAPHONS_LOC)
+        print('loading graphs from ', GRAPHONS_DIR)
         graphs, labels = syn_graphons.load_graphs() 
 
-    # creating embeddings
+    # creating graph2vec embeddings of the graphs from graphons and storing them
     if CREATE_EMBEDDINGS:
         print('creating graph2vec embeddings')
         tmp = np.split(np.array(graphs), NUM_GRAPHONS)
-        embed_all_graph2vec(emb_dir=EMBEDDING_DIR, graph_list=[list(tmp[i]) for i in range(NUM_GRAPHONS)], 
-                                                                data = DATA['SYNTHETIC_DATA'])
+        embed_all_graph2vec(emb_dir=G2V_EMBEDDING_DIR, 
+                            graph_list=[list(tmp[i]) for i in range(NUM_GRAPHONS)], data = DATA[1])
 
-    # classification of graph2vec embeddings
-    embeddings, true_labels = load_embeddings(names=DATA['SYNTHETIC_DATA'])
+    # loading graph2vec embeddings of the graphone
+    embeddings, true_labels = load_graphons_embeddings(embedding_dir=G2V_EMBEDDING_DIR, names=DATA[1])
     embeddings = np.squeeze(embeddings)
-    print('number of labels: ',len(true_labels))
-    classification(embeddings, true_labels)
-    # clustering(embeddings, labels, k = 2, GRAPH2VEC=True)
+
+    print('\nPerforming classification on histogram approximation')
+    classification_train_acc, classification_test_acc = classification(embeddings, true_labels, GRAPH2VEC=True)
+
+    # print('performing clustering on histogram approximation')
+    # clustering_rand_score, clustering_error = clustering(embeddings, labels, k=NUM_GRAPHONS, GRAPH2VEC=True)
+
+    if SWEEP:
+        wandb.log({'g2v_class_train_accuracy': classification_train_acc, 
+                    'g2v_class_test_accuracy': classification_test_acc,
+                    'g2v_clustering_rand_score': clustering_rand_score,
+                    'g2v_clustering_error': clustering_error})
+
 
 
     # classification of graphon embeddings
-    approxs = hist_approximate(graphs, n0=30) 
+    print('creating histogram approximation of graphs')
+    approxs = hist_approximate(graphs, n0=N0) 
     embeddings = []
     for i in range(len(approxs)):
         flattened_emb = approxs[i].numpy().flatten()
         embeddings.append(flattened_emb)
-    train_acc, test_acc = classification(embeddings, labels)
+
+    print('\nPerforming classification on histogram approximation')
+    classification_train_acc, classification_test_acc = classification(embeddings, labels)
+
+    # print('performing clustering on histogram approximation')
+    # clustering_rand_score, clustering_error = clustering(embeddings, labels, k = NUM_GRAPHONS, GRAPH2VEC=False)
+
     if SWEEP:
-        wandb.log({'train_accuracy': train_acc, 
-                    'test_accuracy': test_acc})
-    # clustering(approxs, labels, k = 2)
+        wandb.log({'graphons_class_train_accuracy': classification_train_acc, 
+                    'graphons_class_test_accuracy': classification_test_acc,
+                    'graphons_clustering_rand_score': clustering_rand_score,
+                    'graphons_clustering_error': clustering_error})
+    
+
+
 
 def sweep(config=None):
     with wandb.init(config=config):
-            config = wandb.config
-            clustering_classification(**config)
-
+            clustering_classification(**wandb.config)
 
 
 if __name__ == '__main__':
@@ -92,14 +119,16 @@ if __name__ == '__main__':
     with open("config.yaml", 'r') as stream:
         config_def = yaml.load(stream, Loader=yaml.FullLoader)
 
+    with open('sweep_config.yaml', 'r') as f:
+            sweep_configuration = yaml.load(f, Loader=yaml.FullLoader)
+
     # if we are sweeping, we update the config with the default values and start the sweep
     # else we run the code using the config_def values
     if config_def['SWEEP']:
-        with open('sweep_config.yaml', 'r') as f:
-            sweep_configuration = yaml.load(f, Loader=yaml.FullLoader)
+        wandb.login()
         sweep_configuration = update_sweep_config(sweep_configuration, config_def)
-        sweep_id = wandb.sweep(sweep_configuration, project="graphon")
-        wandb.agent(sweep_id, sweep, count = 5)
+        sweep_id = wandb.sweep(sweep_configuration, project="graphon", entity='seb-graphon')
+        wandb.agent(sweep_id, sweep)
     else:
         clustering_classification(**config_def)
 
