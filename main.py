aafe638eeb4305dfd0fa5cb3db70be96f4f6d33b
simon <bonato@kth.se>
2022-10-17 14:20:56 +0200

    jit
    Co-authored-by: Vishal Nedungadi <vishalned@gmail.com>

--- main.py
+++ main.py
@@ -1,52 +1,15 @@
 from pathlib import Path
 from embedding import create_g2v_embeddings, load_embeddings, histogram_embeddings
 from data_loader.syntheticData import SynthGraphons
-from utils import classification, clustering
+from utils import classification, clustering, update_config, download_datasets, load_graph, combine_datasets
 import numpy as np
 import wandb
 import yaml
+import time
 import os
 
-'''
-Have to make the code in the form of a function that takes in all the needed parameters
-Then when we want to sweep we pass the sweep parameters, else, the normal ones.
-'''
 
-def update_config(sweep_config, config_def):
-    """
-    Updated the sweep config with the default config, adding the default values 
-
-    :param sweep_config: the sweep config to be updated
-    :type sweep_config: dict
-
-    :param config_def: the default config
-    :type config_def: dict
-
-    :return: the updated sweep config
-    :rtype: dict
-    """
-    real_data_params = ['DOWNLOAD_DATA']
-    synth_data_params = ['NUM_GRAPHS_PER_GRAPHON', 'NUM_NODES', 'N0']
-
-    if config_def['SYNTH_DATA']:
-        to_remove = real_data_params
-    else:
-        to_remove = synth_data_params
-    
-    for param in to_remove:
-        if param in sweep_config['parameters']:
-            del sweep_config['parameters'][param]
-        del config_def[param]
-    del config_def['SYNTH_DATA']
-
-    if config_def['SWEEP']:
-        for key, value in config_def.items():
-            if key not in sweep_config['parameters'].keys(): 
-                sweep_config['parameters'][key] = {'value': value}
-        return sweep_config
-    return config_def
-
-def clustering_classification_synth(
+def clustering_classification(
     NUM_GRAPHS_PER_GRAPHON=100,
     NUM_NODES=None,
     N0=30,
@@ -55,26 +18,53 @@ def clustering_classification_synth(
     G2V_EMBEDDING_DIR=None,
     DATA=None,
     SWEEP=False,
+    DOWNLOAD_DATA=False,
+    SYNTH_DATA=True
     
 ):
-    NUM_GRAPHONS = len(DATA[1])
-    parent_dir = Path('graphons_dir')
-    parent_dir.mkdir(exist_ok=True, parents=True)
-    GRAPHONS_DIR = parent_dir.joinpath(f'{NUM_GRAPHONS}_graphons_{NUM_GRAPHS_PER_GRAPHON}_graphs.pkl')
+    if SYNTH_DATA:
+        NUM_GRAPHONS = len(DATA[1])
+        k = NUM_GRAPHONS
+        parent_dir = Path('graphons_dir')
+        parent_dir.mkdir(exist_ok=True, parents=True)
+        GRAPHONS_DIR = parent_dir.joinpath(f'{NUM_GRAPHONS}_graphons_{NUM_GRAPHS_PER_GRAPHON}_graphs.pkl')
 
-    # synthetic data_loader
-    if SAVE_GRAPHONS:
-        syn_graphons = SynthGraphons(NUM_NODES, NUM_GRAPHS_PER_GRAPHON, DATA[1])
-        graphs, true_labels = syn_graphons.data_simulation(start=100, stop=1000)
-        syn_graphons.save_graphs(GRAPHONS_DIR)
+        # synthetic data_loader
+        if SAVE_GRAPHONS:
+            syn_graphons = SynthGraphons(NUM_NODES, NUM_GRAPHS_PER_GRAPHON, DATA[1])
+            graphs, true_labels = syn_graphons.data_simulation(start=100, stop=1000)
+            syn_graphons.save_graphs(GRAPHONS_DIR)
+        else:
+            graphs, true_labels = SynthGraphons.load_graphs(path=GRAPHONS_DIR)
     else:
-        graphs, true_labels = SynthGraphons.load_graphs(path=GRAPHONS_DIR)
+        if DOWNLOAD_DATA:
+            download_datasets()
+        # loading graphs
+        fb = load_graph(min_num_nodes=100, name=DATA[0][0])
+        github = load_graph(min_num_nodes=100, name=DATA[0][2])
+        reddit = load_graph(min_num_nodes=100, name=DATA[0][3])
+        deezer = load_graph(min_num_nodes=100, name=DATA[0][1])
+        # graphs, true_labels = combine_datasets([fb, github, reddit])
+        # graphs, true_labels = combine_datasets([fb, github, deezer])
+        # graphs, true_labels = combine_datasets([fb, reddit, deezer])
+        # graphs, true_labels = combine_datasets([github, reddit, deezer])
+        graphs, true_labels = combine_datasets([fb, github, reddit, deezer])
+
+        k = len(np.unique(true_labels))
+    
+    # start recording time for embedding creation
+    
+    start_t_g2v = time.time()
 
     # creating graph2vec embeddings of the graphs from graphons and storing them
     if CREATE_EMBEDDINGS:
-        print('Creating graph2vec embeddings')
+        print('\nCreating graph2vec embeddings')
         create_g2v_embeddings(graph_list=graphs, true_labels=true_labels, dir_name=G2V_EMBEDDING_DIR)
+    time_g2v = time.time() - start_t_g2v
+    print(f'Graph2vec embeddings created in {time_g2v} seconds')
+    wandb.log({'time_g2v': time_g2v})
 
+    
     # classification of graph2vec embeddings
     embeddings, true_labels = load_embeddings(dir_name=G2V_EMBEDDING_DIR)
     embeddings = np.squeeze(embeddings)
@@ -83,18 +73,19 @@ def clustering_classification_synth(
     print('\nPerforming classification on histogram approximation')
     classification_train_acc, classification_test_acc = classification(embeddings, true_labels, GRAPH2VEC=True)
 
-    # print('performing clustering on histogram approximation')
-    # clustering_rand_score, clustering_error = clustering(embeddings, true_labels, k=NUM_GRAPHONS, GRAPH2VEC=True)
+    print('performing clustering on histogram approximation')
+    clustering_rand_score, clustering_error = clustering(embeddings, true_labels, k=k, GRAPH2VEC=True)
 
     if SWEEP:
         wandb.log({
             'g2v_class_train_accuracy': classification_train_acc, 
-                    'g2v_class_test_accuracy': classification_test_acc,})
-                    # 'g2v_clustering_rand_score': clustering_rand_score,
-                    # 'g2v_clustering_error': clustering_error})
+                    'g2v_class_test_accuracy': classification_test_acc,
+                    'g2v_clustering_rand_score': clustering_rand_score,
+                    'g2v_clustering_error': clustering_error,
+                    'time_g2v': time_g2v})
 
 
-
+    start_t_graphons = time.time()
     # classification of graphon embeddings
     print('creating histogram approximation of graphs')
     hist_embeddings = histogram_embeddings(graphs, n0=N0) 
@@ -102,25 +93,28 @@ def clustering_classification_synth(
     for i in range(len(hist_embeddings)):
         flattened_emb = hist_embeddings[i].numpy().flatten()
         embeddings.append(flattened_emb)
+    time_graphons = time.time() - start_t_graphons
+    print(f'Graphon embeddings created in {time_graphons} seconds')
 
     print('\nPerforming classification on histogram approximation')
     classification_train_acc, classification_test_acc = classification(embeddings, true_labels)
 
-    # print('performing clustering on histogram approximation')
-    # clustering_rand_score, clustering_error = clustering(embeddings, true_labels, k = NUM_GRAPHONS, GRAPH2VEC=False)
+    print('performing clustering on histogram approximation')
+    clustering_rand_score, clustering_error = clustering(embeddings, true_labels, k=k, GRAPH2VEC=False)
 
     if SWEEP:
         wandb.log({
             'graphons_class_train_accuracy': classification_train_acc, 
-                    'graphons_class_test_accuracy': classification_test_acc,})
-                    # 'graphons_clustering_rand_score': clustering_rand_score,
-                    # 'graphons_clustering_error': clustering_error})
+                    'graphons_class_test_accuracy': classification_test_acc,
+                    'graphons_clustering_rand_score': clustering_rand_score,
+                    'graphons_clustering_error': clustering_error,
+                    'time_graphons': time_graphons})
 
     
 
 def sweep(config=None):
     with wandb.init(config=config):
-            clustering_classification_synth(**wandb.config)
+            clustering_classification(**wandb.config)
 
 
 if __name__ == '__main__':
@@ -139,7 +133,13 @@ if __name__ == '__main__':
         sweep_id = wandb.sweep(sweep_configuration, project="graphon", entity='seb-graphon')
         wandb.agent(sweep_id, sweep)
     else:
-        clustering_classification_synth(**final_config)
+        clustering_classification(**final_config)
+
+
+
+
+
+
 
 
 '''
